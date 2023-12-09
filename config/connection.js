2d85d63214809b874c78b2b3f09e934c721bbc3b
WeenoCoding <hernandez17marc@gmail.com>
2023-12-08 21:46:18 -0800

    made changes to env file and conection.js

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
